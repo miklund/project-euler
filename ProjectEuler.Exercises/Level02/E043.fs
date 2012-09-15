fdcb007843dc29deabd1d0ca1b96cba5051ff0f2
Mikael Lundin <mikael.lundin@litemedia.se>
2012-09-15 21:23:39 +0200

    Completed the PE43

--- ProjectEuler.Exercises/Level02/E043.fs
+++ ProjectEuler.Exercises/Level02/E043.fs
@@ -1,8 +1,27 @@
 ﻿module E043
 
-let _pandigitals part left = seq {
-        match left with
-        | [] -> yield part 
-        | head :: rest ->  
-        
-    }
+let rec insertions x = function
+    | []             -> [[x]]
+    | (y :: ys) as l -> (x::l)::(List.map (fun x -> y::x) (insertions x ys))
+
+let rec permutations = function
+    | []      -> seq [ [] ]
+    | x :: xs -> Seq.concat (Seq.map (insertions x) (permutations xs))
+
+let number = List.reduce (fun acc n -> acc * 10 + n)
+let number64 = List.fold (fun acc n -> acc * 10L + int64(n)) 0L
+
+let divisibleBy b a = a % b = 0
+
+let criteria v =
+    let [_; v2; v3; v4; v5; v6; v7; v8; v9; v10] = v
+    number([v2; v3; v4]) |> divisibleBy 2 &&
+    number([v3; v4; v5]) |> divisibleBy 3 &&
+    number([v4; v5; v6]) |> divisibleBy 5 &&
+    number([v5; v6; v7]) |> divisibleBy 7 &&
+    number([v6; v7; v8]) |> divisibleBy 11 &&
+    number([v7; v8; v9]) |> divisibleBy 13 &&
+    number([v8; v9; v10]) |> divisibleBy 17
+
+// very brute forceish
+let solution = permutations [0..9] |> Seq.filter criteria |> Seq.fold (fun acc n -> acc + number64(n)) 0L
---
+++ ProjectEuler.Exercises/Level02/E043b.fs
@@ -0,0 +1,56 @@
+﻿module E043b
+
+// wide depth lft rgt
+// depth: the length of result lists
+// lft: the left list (default [])
+// rgt: the right list (default items we want to permute)
+//
+// example: wide 2 [] [1; 2; 3]
+// -> [[1; 2]; [1; 3]; [2; 1]; [2; 3]; [3; 2]; [3; 1]]
+let rec wide depth lft = function
+    | []       -> []
+    | hd :: tl -> (deep hd (depth, lft @ tl)) @ (wide depth (hd :: lft) tl)
+
+and deep x = function
+    | 1, _ | _, [] -> [[x]]
+    | d, l         -> List.map (fun i -> x :: i) (wide (d - 1) [] l)
+
+// permute length items, utility method to wide/deep
+//
+// example: permute 2 [1..3]
+// -> [[1; 2]; [1; 3]; [2; 1]; [2; 3]; [3; 2]; [3; 1]]
+let permute length = wide length []
+
+// divisibleBy 7 14
+// -> true
+let divisibleBy d n = n % d = 0
+
+// toNumber [1; 2; 3]
+// -> 123
+let toNumber = List.reduce (fun acc n -> acc * 10 + n)
+
+// toLog [1; 2; 3]
+// -> 123L
+let toLong = List.fold (fun acc n -> acc * 10L + int64(n)) 0L
+
+// difference [1..3] [1; 3]
+// [2]
+let difference left right = left |> List.filter (fun n -> right |> List.exists ((=) n) |> not)
+
+// appendPandigital [[1; 3; 5; 7; 9]]
+// -> [[0; 1; 3; 5; 7; 9]; [2; 1; 3; 5; 7; 9]; [4; 1; 3; 5; 7; 9]; [6; 1; 3; 5; 7; 9]; [8; 1; 3; 5; 7; 9]]
+let appendPandigital = List.map (fun pandigital -> List.map (fun i -> i :: pandigital) (difference [0..9] pandigital)) >> List.concat
+
+// take 3 [1..9]
+let rec take n l = match n, l with | 0, _ -> [] | n, x :: xs -> x :: take (n - 1) xs
+
+// filterDivisibleBy 2 [[1]; [2]; [3]; [4]; [5]; [6]; [7]; [8]; [9]]
+// -> [[2]; [4]; [6]; [8]]
+let filterDivisibleBy n = List.filter (take 3 >> toNumber >> divisibleBy n)
+
+// work from behind, all permutations of [0..9] with 3 length
+// filter divisible by 17, 13, 11, 7, 5, 3, 2 and append a new pandigital for each iteration
+let solution = [17; 13; 11; 7; 5; 3; 2] 
+               |> List.fold (fun acc n -> acc |> filterDivisibleBy n |> appendPandigital) (permute 3 [0..9])
+               |> List.map (toLong)
+               |> List.sum
